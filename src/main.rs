650763ebc5ca5e5a3859870e8c2075bb46ae64d9
Élise <elise@eiss.fr>
2024-07-29 16:29:48 +0200

    potator -> timetable.rs

--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,10 @@
 mod model;
-use std::{collections::HashMap, sync::Arc};
+mod timetable;
 
-use multimap::MultiMap;
 use spinoff::{spinners, Spinner};
+use timetable::Timetable;
 
-fn gtfs_stuff() {
-    for arg in std::env::args().skip(1) {
-        if let std::ops::ControlFlow::Break(_) = gtfs_parse_and_explore(arg) {
-            return;
-        }
-    }
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-struct Potator {
-    pub now: chrono::NaiveDateTime,
-    pub today: chrono::NaiveDate,
-    pub current_time: chrono::NaiveTime,
-    pub calendar: HashMap<String, gtfs_structures::Calendar>,
-    pub calendar_dates: HashMap<String, Vec<gtfs_structures::CalendarDate>>,
-    pub stops: HashMap<String, gtfs_structures::Stop>,
-    pub routes: HashMap<String, gtfs_structures::Route>,
-    pub trips: MultiMap<String, Trip>,
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-struct Trip {
-    pub id: String,
-    pub service_id: String,
-    pub route_id: String,
-    pub stop_times: Vec<StopTime>,
-}
-
-impl From<&gtfs_structures::Trip> for Trip {
-    fn from(value: &gtfs_structures::Trip) -> Self {
-        Self {
-            id: value.id.clone(),
-            service_id: value.service_id.clone(),
-            route_id: value.route_id.clone(),
-            stop_times: value
-                .stop_times
-                .iter()
-                .filter_map(|item| StopTime::try_from(item).ok())
-                .collect(),
-        }
-    }
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-struct StopTime {
-    pub time: chrono::NaiveTime,
-    pub stop_id: String,
-    pub name: String,
-}
-
-impl TryFrom<&gtfs_structures::StopTime> for StopTime {
-    type Error = &'static str;
-    fn try_from(value: &gtfs_structures::StopTime) -> Result<Self, Self::Error> {
-        let time = chrono::NaiveTime::from_num_seconds_from_midnight_opt(
-            value
-                .departure_time
-                .or(value.arrival_time)
-                .ok_or("no arrival or departure time on stop")?,
-            0,
-        )
-        .ok_or("could not convert arival/departure time to chrono::NaiveTime")?;
-        Ok(Self {
-            time,
-            stop_id: value.stop.id.clone(),
-            name: value.stop.name.clone().ok_or("stop without a name")?,
-        })
-    }
-}
-
-impl Potator {
-    fn new() -> Self {
-        let now = Local::now();
-        // println!("{now:#?}");
-        let now_naive: chrono::NaiveDateTime = now.naive_local();
-        Self {
-            now: now_naive,
-            today: now_naive.date(),
-            current_time: now_naive.time(),
-            calendar: HashMap::new(),
-            calendar_dates: HashMap::new(),
-            stops: HashMap::new(),
-            routes: HashMap::new(),
-            trips: MultiMap::new(),
-        }
-    }
-
-    fn runs_on_weekday(&self, gtfs_cal: &gtfs_structures::Calendar) -> bool {
-        let date = self.today;
-        // dbg!(date);
-        // dbg!(gtfs_cal);
-        if date < gtfs_cal.start_date || date > gtfs_cal.end_date {
-            return false;
-        }
-        let day_of_week = self.now.weekday();
-        match day_of_week {
-            chrono::Weekday::Mon if gtfs_cal.monday => true,
-            chrono::Weekday::Tue if gtfs_cal.tuesday => true,
-            chrono::Weekday::Wed if gtfs_cal.wednesday => true,
-            chrono::Weekday::Thu if gtfs_cal.thursday => true,
-            chrono::Weekday::Fri if gtfs_cal.friday => true,
-            chrono::Weekday::Sat if gtfs_cal.saturday => true,
-            chrono::Weekday::Sun if gtfs_cal.sunday => true,
-            _ => false,
-        }
-    }
-}
-
-fn gtfs_parse_and_explore(arg: String) -> std::ops::ControlFlow<()> {
+fn gtfs_extract(arg: String) -> std::ops::ControlFlow<()> {
     let mut spinner = Spinner::new(spinners::Dots, format!("Parsing GTFS of: {arg}"), None);
     let gtfs = match gtfs_structures::Gtfs::new(&arg) {
         Ok(val) => val,
@@ -122,7 +15,7 @@ fn gtfs_parse_and_explore(arg: String) -> std::ops::ControlFlow<()> {
     };
     spinner.success("Parsing complete");
     println!("there are {} stops in the gtfs", gtfs.stops.len());
-    let mut potator = Potator::new();
+    let mut potator = Timetable::new();
     for (id, route) in gtfs.routes.iter().filter(|route| route.0 == "IDFM:C02298") {
         potator.routes.insert(id.clone(), route.clone());
         println!("route id {id}");
@@ -172,12 +65,19 @@ fn gtfs_parse_and_explore(arg: String) -> std::ops::ControlFlow<()> {
     std::ops::ControlFlow::Continue(())
 }
 
-use chrono::prelude::*;
 fn main() {
-    gtfs_stuff();
+    gtfs_by_arg();
     // let now = std::time::SystemTime::now();
     // println!("{now:#?}");
     // let cc = Local::now();
     // println!("{cc:#?}");
     // let dd: chrono::NaiveDateTime = cc.naive_local();
+}
+
+fn gtfs_by_arg() {
+    for arg in std::env::args().skip(1) {
+        if let std::ops::ControlFlow::Break(_) = gtfs_extract(arg) {
+            return;
+        }
+    }
 }
---
+++ src/timetable.rs
@@ -0,0 +1,103 @@
+use multimap::MultiMap;
+use std::collections::HashMap;
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Timetable {
+    pub now: chrono::NaiveDateTime,
+    pub today: chrono::NaiveDate,
+    pub current_time: chrono::NaiveTime,
+    pub calendar: HashMap<String, gtfs_structures::Calendar>,
+    pub calendar_dates: HashMap<String, Vec<gtfs_structures::CalendarDate>>,
+    pub stops: HashMap<String, gtfs_structures::Stop>,
+    pub routes: HashMap<String, gtfs_structures::Route>,
+    pub trips: MultiMap<String, Trip>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Trip {
+    pub id: String,
+    pub service_id: String,
+    pub route_id: String,
+    pub stop_times: Vec<StopTime>,
+}
+
+impl From<&gtfs_structures::Trip> for Trip {
+    fn from(value: &gtfs_structures::Trip) -> Self {
+        Self {
+            id: value.id.clone(),
+            service_id: value.service_id.clone(),
+            route_id: value.route_id.clone(),
+            stop_times: value
+                .stop_times
+                .iter()
+                .filter_map(|item| StopTime::try_from(item).ok())
+                .collect(),
+        }
+    }
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct StopTime {
+    pub time: chrono::NaiveTime,
+    pub stop_id: String,
+    pub name: String,
+}
+
+impl TryFrom<&gtfs_structures::StopTime> for StopTime {
+    type Error = &'static str;
+    fn try_from(value: &gtfs_structures::StopTime) -> Result<Self, Self::Error> {
+        let time = chrono::NaiveTime::from_num_seconds_from_midnight_opt(
+            value
+                .departure_time
+                .or(value.arrival_time)
+                .ok_or("no arrival or departure time on stop")?,
+            0,
+        )
+        .ok_or("could not convert arival/departure time to chrono::NaiveTime")?;
+        Ok(Self {
+            time,
+            stop_id: value.stop.id.clone(),
+            name: value.stop.name.clone().ok_or("stop without a name")?,
+        })
+    }
+}
+
+use chrono::prelude::*;
+
+impl Timetable {
+    pub fn new() -> Self {
+        let now = Local::now();
+        // println!("{now:#?}");
+        let now_naive: chrono::NaiveDateTime = now.naive_local();
+        Self {
+            now: now_naive,
+            today: now_naive.date(),
+            current_time: now_naive.time(),
+            calendar: HashMap::new(),
+            calendar_dates: HashMap::new(),
+            stops: HashMap::new(),
+            routes: HashMap::new(),
+            trips: MultiMap::new(),
+        }
+    }
+
+    pub fn runs_on_weekday(&self, gtfs_cal: &gtfs_structures::Calendar) -> bool {
+        let date = self.today;
+        // dbg!(date);
+        // dbg!(gtfs_cal);
+        if date < gtfs_cal.start_date || date > gtfs_cal.end_date {
+            return false;
+        }
+        let day_of_week = self.now.weekday();
+        match day_of_week {
+            chrono::Weekday::Mon if gtfs_cal.monday => true,
+            chrono::Weekday::Tue if gtfs_cal.tuesday => true,
+            chrono::Weekday::Wed if gtfs_cal.wednesday => true,
+            chrono::Weekday::Thu if gtfs_cal.thursday => true,
+            chrono::Weekday::Fri if gtfs_cal.friday => true,
+            chrono::Weekday::Sat if gtfs_cal.saturday => true,
+            chrono::Weekday::Sun if gtfs_cal.sunday => true,
+            _ => false,
+        }
+    }
+}
